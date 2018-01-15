752e34ee0adf58de6a2e6d163222ce35e199c5eb
Spd <2580744@gmail.com>
2018-01-15 17:09:22 +0300

    aw

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-cache = require('gulp-cache'); // Подключаем библиотеку кеширования
-var imageminJpegoptim = require('imagemin-jpegoptim');
+
 gulp.task('mytask', function() {
     console.log('Привет, я таск!');
 });
@@ -16,15 +13,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
         notify: false // Отключаем уведомления
     });
 });
-gulp.task('img', function() {
-    return gulp.src('app/img/**/*') // Берем все изображения из app
-        .pipe(cache(imagemin(['app/img/*.jpg'], 'build/images', {
-                use: [
-                    imageminJpegoptim()
-                ]
-              })))
-        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
-});
+
 
 
 
@@ -36,14 +25,14 @@ gulp.task('autopref', function() {
         }))
         .pipe(gulp.dest('dist'));
 });
-gulp.task('watch', ['browser-sync', 'autopref', 'img'], function() {
+gulp.task('watch', ['browser-sync', 'autopref'], function() {
     gulp.watch('app/css/*.css', ['autopref', browserSync.reload]); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 });
 gulp.task('default', ['watch']);
 
-gulp.task('build', [ 'img'], function() {
+gulp.task('build', function() {
 
     var buildCss = gulp.src([ // Переносим библиотеки в продакшен
             'app/css/*.css'
@@ -52,6 +41,8 @@ gulp.task('build', [ 'img'], function() {
 
     var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
         .pipe(gulp.dest('dist/fonts'))
+var buildImg = gulp.src('img/**/*') // Переносим img в продакшен
+        .pipe(gulp.dest('dist/img'))
 
     var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
         .pipe(gulp.dest('dist/js'))
